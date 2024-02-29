353b6b2b926614b54b968e0e39b1bab1e471c481
khanhtran-er <khanh.tran.er@outlook.com>
2024-02-29 15:14:25 +0700

    assets

--- main.go
+++ main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
-    "net/http"
+	"fmt"
+	"net/http"
+    "io/ioutil"
+    "log"
 )
 
-func main() {
-    // Serve the HTML file at the root ("/")
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "static/index.html")
-    })
+func handleAssetRequest(w http.ResponseWriter, r *http.Request) {
 
-    // Start the server on port 8080
-    err := http.ListenAndServe(":8080", nil)
+    buf, err := ioutil.ReadFile("assets/gobin_icon.png")
+
     if err != nil {
-        panic(err)
+        log.Fatal(err)
     }
+
+    w.Header().Set("Content-Type", "image/png")
+    w.Write(buf)
+}
+
+func main() {
+	// Serve the HTML file at the root ("/")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL)
+		http.ServeFile(w, r, "static/index.html")
+	})
+
+	http.HandleFunc("/assets/gobin-icon", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Println(r.URL.Path)
+		http.ServeFile(w, r, "assets/gobin_icon.png")
+	})
+
+	// Start the server on port 8080
+	fmt.Println("Start the server on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
